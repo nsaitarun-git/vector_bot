f05739124bad239d0548a7a4722ed2eb5d0184a4
nsaitarun-git <saitarun.nadipineni@gmail.com>
2024-06-10 13:46:29 +0100

    Nav Update

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -14,15 +14,15 @@ def generate_launch_description():
     joy_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params],
+            parameters=[joy_params, {'use_sim_time': use_sim_time}],
          )
     
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
-            parameters=[joy_params],
-            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
     twist_stamper = Node(
@@ -36,5 +36,5 @@ def generate_launch_description():
     return LaunchDescription([
         joy_node,    
         teleop_node,
-        twist_stamper
+        #twist_stamper
     ])
